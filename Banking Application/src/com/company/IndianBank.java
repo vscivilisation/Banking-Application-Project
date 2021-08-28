09d423faceb05d0a45f724d93d3f8a043a97f74d
vscivilisation <54363139+vscivilisation@users.noreply.github.com>
2021-08-28 17:32:17 +0530

    Add files via upload

---
+++ Banking Application/src/com/company/IndianBank.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class IndianBank {
+    Account Vaibhav = new Account("Vaibhav Srivastava", "A00001");
+
+
+    Account Kartik = new Account("Kartik", "B00002");
+		Kartik.showMenu();
+
+}
